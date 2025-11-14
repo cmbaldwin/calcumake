31fc9dfcf46e660d733b83317d616e03c7b2e3d2
cmbaldwin <codybaldwin@gmail.com>
2025-11-14 13:23:50 +0900

    Working on implementing modal pop-ups for on-the-fly record adding on invoices, print pricings and print pricing plates

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers", preload: false
-pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/esm/index.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.esm.min.js"
 pin "jspdf", to: "https://esm.sh/jspdf@3.0.3"
 pin "html2canvas", to: "https://esm.sh/html2canvas@1.4.1"
