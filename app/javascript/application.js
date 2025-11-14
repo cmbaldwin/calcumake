86b3476c7b80d864352a91a4f03c4cd75cbb60f9
cmbaldwin <codybaldwin@gmail.com>
2025-11-14 13:23:50 +0900

    Working on implementing modal pop-ups for on-the-fly record adding on invoices, print pricings and print pricing plates

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "bootstrap"
+
+// Import Bootstrap ESM module and make it globally available
+import * as bootstrap from "bootstrap"
+window.bootstrap = bootstrap
